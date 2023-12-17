2a6c4d801243b4b504967df5707ba7cdbd1aa93a
Koichi5 <koichi20021217@gmail.com>
2023-12-18 02:11:17 +0900

    edit robot cleaner model animation

--- RobotCleanerNumRealityArea.swift
+++ RobotCleanerNumRealityArea.swift
@@ -10,15 +10,15 @@ import RealityKit
 import RealityKitContent
 
 struct RobotCleanerNumRealityArea: View {
-    @Environment(\.openWindow) private var openWindow
-    @Environment(\.dismissWindow) private var dismissWindow
-    @ObservedObject private var model = AreaViewModel()
     @State var isBehind: Bool = false
     let attachmentID = "attachmentID"
     
     var body: some View {
         RealityView { content, attachments in
-            guard let entity = try? await Entity(named: "RobotCleanerNumScene", in: realityKitContentBundle) else {
+//            guard let entity = try? await Entity(named: "RobotCleanerNumScene", in: realityKitContentBundle) else {
+//                fatalError("Unalbe to load scene model")
+//            }
+            guard let entity = try? await Entity(named: "Scene", in: realityKitContentBundle) else {
                 fatalError("Unalbe to load scene model")
             }
             guard let env = try? await EnvironmentResource(named: "Directional") else { return }
@@ -30,27 +30,46 @@ struct RobotCleanerNumRealityArea: View {
             content.add(entity)
             
             if let sceneAttachment = attachments.entity(for: attachmentID) {
-//                sceneAttachment.position = isBehind ? SIMD3<Float>(0.1, 0, 0) : SIMD3<Float>(-0.1, 0, 0)
-//                sceneAttachment.transform.rotation = simd_quatf(angle: -0.5, axis: SIMD3<Float>(1,0,0))
                 content.add(sceneAttachment)
             }
         } update: { content, attachments in
+//            if let entity = content.entities.first {
+//                let uniformPosition = isBehind ? SIMD3<Float>(-0.17, 0, 0) : SIMD3<Float>(0.17, 0, 0)
+//                let uniformRotate = isBehind ? simd_quatf(angle: .pi, axis: SIMD3<Float>(0, 1, 0)) : simd_quatf(angle: 0, axis: SIMD3<Float>(0, 1, 0))
+//                
+//                entity.transform.rotation = uniformRotate
+//                entity.position = uniformPosition
+//            }
+//            
+//            if let attachment = attachments.entity(for: attachmentID) {
+//                let uniformPosition = isBehind ? SIMD3<Float>(0.17, 0, 0) : SIMD3<Float>(-0.17, 0, 0)
+//                attachment.position = uniformPosition
+//            }
+            
             if let entity = content.entities.first {
-//                let ix: Float = 0.0
-//                let iy: Float = isBehind ? 0.707107 : 0.0
-//                let iz: Float = 0.0
-//                let r: Float = isBehind ? 0.707107 : 0.0
-                let uniformPosition = isBehind ? SIMD3<Float>(-0.17, 0, 0) : SIMD3<Float>(0.17, 0, 0)
-                let uniformRotate = isBehind ? simd_quatf(angle: .pi, axis: SIMD3<Float>(0, 1, 0)) : simd_quatf(angle: 0, axis: SIMD3<Float>(0, 1, 0))
+                let newPosition = isBehind ? SIMD3<Float>(-0.17, 0, 0) : SIMD3<Float>(0.17, 0, 0)
+                let newRotation = isBehind ? simd_quatf(angle: .pi, axis: SIMD3<Float>(0, 1, 0)) : simd_quatf(angle: 0, axis: SIMD3<Float>(0, 1, 0))
                 
-                entity.transform.rotation = uniformRotate
-                entity.position = uniformPosition
+                // アニメーションの設定
+                var transform = entity.transform
+                transform.translation = newPosition
+                transform.rotation = newRotation
+
+                // 3秒かけてアニメーション
+                entity.move(to: transform, relativeTo: entity.parent, duration: 3, timingFunction: .easeInOut)
             }
             
             if let attachment = attachments.entity(for: attachmentID) {
-                let uniformPosition = isBehind ? SIMD3<Float>(0.17, 0, 0) : SIMD3<Float>(-0.17, 0, 0)
-                attachment.position = uniformPosition
+                let newPosition = isBehind ? SIMD3<Float>(0.17, 0, 0) : SIMD3<Float>(-0.17, 0, 0)
+                
+                // アニメーションの設定
+                var transform = Transform.identity
+                transform.translation = newPosition
+                
+                // 3秒かけてアニメーション
+                attachment.move(to: transform, relativeTo: attachment.parent, duration: 3, timingFunction: .easeInOut)
             }
+            
         } placeholder: {
             ProgressView()
                 .progressViewStyle(.circular)
@@ -58,43 +77,60 @@ struct RobotCleanerNumRealityArea: View {
         } attachments: {
             Attachment(id: attachmentID) {
                 VStack {
-                    Text("名称一覧")
+//                    Text("名称一覧")
+//                        .font(.title)
+//                    if isBehind {
+//                        VStack(alignment: .leading, spacing: 20) {
+//                            Text("① クリーナーブラシ")
+//                                .font(.headline)
+//                            Text("② 段差センサー")
+//                                .font(.headline)
+//                            Text("③ 後輪部分")
+//                                .font(.headline)
+//                            Text("④ 吸い込み部分")
+//                                .font(.headline)
+//                            Text("⑤ 前輪部分")
+//                                .font(.headline)
+//                        }
+//                        .frame(height: 300)
+//                    } else {
+//                        VStack(alignment: .leading, spacing: 20) {
+//                            Text("① 電源ボタン")
+//                                .font(.headline)
+//                            Text("② ホームボタン")
+//                                .font(.headline)
+//                            Text("③ バンパー")
+//                                .font(.headline)
+//                        }
+//                        .frame(height: 300)
+//                    }
+//                    Button(action: {
+//                        isBehind.toggle()
+//                    }) {
+//                        isBehind ? Text("表面を見る") : Text("裏面を見る")
+//                    }
+//                    Button(action: {
+//                        openWindow(id: model.robotCleanerContentAreaId)
+//                        dismissWindow(id: model.robotCleanerNumId)
+//                    }) {
+//                        Text("Close")
+//                    }
+                    
+                    Text("飛行機のモデル")
                         .font(.title)
                     if isBehind {
-                        VStack(alignment: .leading, spacing: 20) {
-                            Text("① クリーナーブラシ")
-                                .font(.headline)
-                            Text("② 段差センサー")
-                                .font(.headline)
-                            Text("③ 後輪部分")
-                                .font(.headline)
-                            Text("④ 吸い込み部分")
-                                .font(.headline)
-                            Text("⑤ 前輪部分")
-                                .font(.headline)
-                        }
-                        .frame(height: 300)
+                        Text("後側")
+                            .font(.headline)
+                            .padding()
                     } else {
-                        VStack(alignment: .leading, spacing: 20) {
-                            Text("① 電源ボタン")
-                                .font(.headline)
-                            Text("② ホームボタン")
-                                .font(.headline)
-                            Text("③ バンパー")
-                                .font(.headline)
-                        }
-                        .frame(height: 300)
+                        Text("前側")
+                            .font(.headline)
+                            .padding()
                     }
                     Button(action: {
                         isBehind.toggle()
                     }) {
-                        isBehind ? Text("表面を見る") : Text("裏面を見る")
-                    }
-                    Button(action: {
-                        openWindow(id: model.robotCleanerContentAreaId)
-                        dismissWindow(id: model.robotCleanerNumId)
-                    }) {
-                        Text("Close")
+                        isBehind ? Text("前側を見る") : Text("後側を見る")
                     }
                 }
             }
